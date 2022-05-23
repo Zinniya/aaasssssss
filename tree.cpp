fd53f48c623d3ae061cc2c88c1079f6ea22715c6
Taffannum <98430006+Zinniya@users.noreply.github.com>
2022-05-23 12:17:09 +0600

    EVEN AND ODD

--- tree.cpp
+++ tree.cpp
@@ -24,3 +24,36 @@ int main()
    }
    return 0;
 }
+  
+
+
+
+
+
+
+EVEN AND ODD
+   #include <iostream>
+
+using namespace std;
+
+int main()
+{
+   int arr[]={1,5,8,10,66,9,44};
+   int n;
+   n=sizeof(arr)/sizeof(arr[0]);
+   int even =0;
+   int odd =0;
+   for (int i=0;i<n;i++){
+       if (arr[i]%2==0){
+           even=even+1;
+       }
+       else{
+           odd=odd+1;
+       }
+   }
+   cout<<"EVEN NUMBERS"<<endl;
+   cout<<even<<endl;
+      cout<<"ODD NUMBERS"<<endl;
+   cout<<odd<<endl;  
+   return 0;
+}
